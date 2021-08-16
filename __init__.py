0a1b9848ffef96dee3a990c62953f41e15cefb96
leegakyeong <alaska7132@naver.com>
2021-08-16 17:33:36 +0900

    Update __init__.py - Add CORS setting - Update character selection params

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from flask_socketio import SocketIO, join_room,emit, rooms, send,close_room
 import random
 import json
 import os
+from flask_cors import CORS
 
 Player_list = {}
 class Player():
@@ -14,43 +15,35 @@ class Player():
         self.hairC = hairC
         self.skin = skin
         self.cloth = cloth
-    
+
     def return_path(self):
         return self.skin+"_"+self.hairC+"_"+self.cloth+"_"+self.hairS+"_"+self.faceS
 
 
-
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='')
+CORS(app, resources={r'*': {'origins': 'http://localhost:5000'}})
 
 @app.route("/")
 def base():
     return send_from_directory('cuberoom-main/public','index.html')
 
 
-@app.route("/<path:path>")
+@app.route("/<path:path>", methods=['GET', 'POST'])
 def home(path):
-    return send_from_directory('cuberoom-main/public',path)
+    return send_from_directory('public', path)
 
 
 
-@app.route("/character-selection",methods=['POST'])
+@app.route("/character-selection",methods=['GET', 'POST'])
 def user_information():
-    name = request.form['name']
-    names  = name.split('.')
-    faceS = request.get_json("faceS")
-    hairS = request.get_json("hairS")
-    hairC = request.get_json("hairC")
-    skin =  request.get_json("skin")
-    cloth = request.get_json("cloth")
-    #faceS,hairS,hairC,skin,cloth = request.form.getlist('')
-    Player_list[request.sid] = Player(name,faceS,hairS,hairC,skin,cloth)
-    filePath = "/faceS"+faceS+"_hairS"+hairS+"_hairC"+hairC+"_hairS"+hairS+"_skin"+skin+"_cloth"+cloth+"/"
-   ##return jsonify(faceS = request.get_json("faceS"),
-   ##                 hairS = request.get_json("hairS"),
-   ##                 hairC = request.get_json("hairC"),
-   ##                 skin =  request.get_json("skin"),
-   ##                 cloth = request.get_json("cloth"),)
-    return  filePath
+    name = request.get_json()["name"]
+    faceS = request.get_json()["faceS"]
+    hairS = request.get_json()["hairS"]
+    hairC = request.get_json()["hairC"]
+    skin =  request.get_json()["skin"]
+    cloth = request.get_json()["cloth"]
+    filePath = f"/skin{skin}_hairC{hairC}_cloth{cloth}_hairS{hairS}_faceS{faceS}/"
+    return url_for('static', filename=filePath)
 
 
 if __name__ == "__main__":
